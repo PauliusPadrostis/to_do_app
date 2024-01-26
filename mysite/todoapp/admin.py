bdbdb8dc30f1acd53e7a3bb6c72dbe2c1e19e4a3
Paulius Padrostis <paulius.padrostis94@gmail.com>
2024-01-26 15:36:10 +0200

    Added meta class and str method to model. Wrote TaskAdmin class to display the tasks in the admin page.

--- mysite/todoapp/admin.py
+++ mysite/todoapp/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['creation_datetime', 'task', 'user', 'due_date', 'status']
+
+    def creation_datetime(self, obj):
+        return obj.creation_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    creation_datetime.short_description = 'Creation Date & Time'
+
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
--- mysite/todoapp/models.py
+++ mysite/todoapp/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
     task = models.CharField(verbose_name='Task', max_length=200)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    creation_date = models.DateField(verbose_name='Task created', auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(verbose_name='Task created', auto_now_add=True)
     due_date = models.DateField(verbose_name='Due on', blank=True, null=True)
 
     COMPLETION_STATUS = (
@@ -16,3 +16,10 @@ class Task(models.Model):
         ('co', 'Completed'),
     )
     status = models.CharField(max_length=2, choices=COMPLETION_STATUS, default='td', help_text='Task status')
+
+    def __str__(self):
+        return self.task
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
