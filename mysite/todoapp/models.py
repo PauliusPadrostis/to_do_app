2746d261df08458dcdbe18ec0b32ded7addbcb34
Paulius Padrostis <paulius.padrostis94@gmail.com>
2024-01-26 15:01:04 +0200

    Basic "Task" model created. Registered on admin.py. Migrated.

--- mysite/todoapp/models.py
+++ mysite/todoapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(verbose_name='Task', max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    creation_date = models.DateField(verbose_name='Task created', auto_now_add=True)
+    due_date = models.DateField(verbose_name='Due on', blank=True, null=True)
+
+    COMPLETION_STATUS = (
+        ('td', 'To do'),
+        ('ip', 'In Progress'),
+        ('ca', 'Cancelled'),
+        ('co', 'Completed'),
+    )
+    status = models.CharField(max_length=2, choices=COMPLETION_STATUS, default='td', help_text='Task status')
